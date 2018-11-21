f2debd990892e9230c754f7755a148bb9aa08651
Nicolas Cavigneaux <nico@bounga.org>
2018-11-21 18:55:59 +0100

    Build full snake and make it move

--- lib/scenes/home.ex
+++ lib/scenes/home.ex
@@ -9,43 +9,61 @@ defmodule Scenake.Scene.Home do
   @tile_size 32
   @snake_starting_size 5
   @tile_radius 8
+  @frame_ms 192
 
   # Initialize the game scene
   def init(_arg, opts) do
     viewport = opts[:viewport]
 
-    # calculate the transform that centers the snake in the viewport
+    # Calculate the transform that centers the snake in the viewport
     {:ok, %ViewPort.Status{size: {vp_width, vp_height}}} = ViewPort.info(viewport)
-    # how many tiles can the viewport hold in each dimension?
+    # How many tiles can the viewport hold in each dimension?
     vp_tile_width = trunc(vp_width / @tile_size)
     vp_tile_height = trunc(vp_height / @tile_size)
 
-    # snake always starts centered
+    # Snake always starts centered
     snake_start_coords = {
       trunc(vp_tile_width / 2),
       trunc(vp_tile_height / 2)
     }
 
-    # The entire game state will be held here
+    # Game loop timer
+    {:ok, timer} = :timer.send_interval(@frame_ms, :frame)
+
+    # Initial game state
     state = %{
       viewport: viewport,
       tile_width: vp_tile_width,
       tile_height: vp_tile_height,
       graph: @graph,
       score: 0,
+      frame_count: 1,
+      frame_timer: timer,
       # Game objects
       objects: %{
         snake: %{body: [snake_start_coords], size: @snake_starting_size, direction: {1, 0}}
       }
     }
 
-    # Update the graph and push it to be rendered
+    update_graph(state)
+
+    {:ok, state}
+  end
+
+  def handle_info(:frame, %{frame_count: frame_count} = state) do
+    state = move_snake(state)
+
+    update_graph(state)
+
+    {:noreply, %{state | frame_count: frame_count + 1}}
+  end
+
+  # Update the graph and push it to be rendered
+  defp update_graph(state) do
     state.graph
     |> draw_score(state.score)
     |> draw_game_objects(state.objects)
     |> push_graph()
-
-    {:ok, state}
   end
 
   # Draw the score HUD
@@ -72,5 +90,19 @@ defmodule Scenake.Scene.Home do
   defp draw_tile(graph, x, y, opts) do
     tile_opts = Keyword.merge([fill: :white, translate: {x * @tile_size, y * @tile_size}], opts)
     graph |> rrect({@tile_size, @tile_size, @tile_radius}, tile_opts)
+  end
+
+  # Move the snake to its next position according to the direction. Also limits the size.
+  defp move_snake(%{objects: %{snake: snake}} = state) do
+    [head | _] = snake.body
+    new_head_pos = move(state, head, snake.direction)
+
+    new_body = Enum.take([new_head_pos | snake.body], snake.size)
+
+    put_in(state, [:objects, :snake, :body], new_body)
+  end
+
+  defp move(%{tile_width: w, tile_height: h}, {pos_x, pos_y}, {vec_x, vec_y}) do
+    {rem(pos_x + vec_x + w, w), rem(pos_y + vec_y + h, h)}
   end
 end
